cc965f22a248ff867038413698336868c63e94a8
Dennis Schafroth <dennis@schafroth.dk>
2018-08-16 18:08:36 +0200

    conversion of log lines into date group and representable as epoch og string.

---
+++ Sources/MailLogCount/DateConversion.swift
@@ -0,0 +1,97 @@
+//
+//  DateConversion.swift
+//  Basic
+//
+//  Created by Dennis Schafroth on 16/08/2018.
+//
+
+import Foundation
+
+class DateConversion {
+    
+    private let interval_str = [9 : ":00", 10: ":0", 11 : "0", 12 : ""]
+    var interval_dict = [ 9 : 3600.0, 11 : 600.0, 12 : 60.0]
+    private var df : DateFormatter;
+    public var len : Int
+    
+    init(df: DateFormatter, len: Int) {
+        self.df = df
+        self.len = len
+        let format = df.dateFormat!
+        df.dateFormat = String(format[..<format.index(format.startIndex, offsetBy: len)])
+        print(df.dateFormat)
+    }
+    
+    public func convert(_ dateString: String) -> Double {
+        let newTime = dateString + interval_str[len]!
+        if let date = df.date(from: dateString) {
+            return date.timeIntervalSince1970
+        }
+        print("convert: Failure: \(newTime) using formatter \(df.dateFormat!) ")
+        return 0.0
+    }
+    
+    func dateFormat(_ date: Date) -> String {
+        let ds = df.string(from: date)
+        
+        let dateStr =  String(ds[..<ds.index(ds.startIndex, offsetBy: len)])
+        // Fix log files uses single digit day of month with double space
+        let index = dateStr.index(dateStr.startIndex, offsetBy: 4)
+        if dateStr[index] == "0" {
+            return dateStr.replacingCharacters(in: index...index, with: " ")
+        }
+        return dateStr
+    }
+
+    func getEpochInterval() -> Double {
+        return interval_dict[len]!
+    }
+
+    func substr(_ line : String) -> String {
+        let end = line.index(line.startIndex, offsetBy: len)
+        return String(line[..<end])
+    }
+}
+
+class DateGroup {
+    private var dc : DateConversion
+    private var epoch : Double
+    private var asString : String;
+
+    init(date: Date, dateConversion : DateConversion) {
+        dc = dateConversion
+        self.epoch = date.timeIntervalSince1970
+        self.asString = dc.dateFormat(date)
+    }
+
+    init(string: String, dateConversion : DateConversion) {
+        dc = dateConversion
+        self.epoch =  dc.convert(string)
+        self.asString = string
+    }
+
+    init(substr: String, dateConversion : DateConversion) {
+        dc = dateConversion
+        self.asString = dc.substr(substr)
+        self.epoch =  dc.convert(self.asString)
+    }
+
+    public func getDateAsEpoch() -> Double {
+        return epoch
+    }
+
+    public func getDateAsString() -> String {
+        return asString
+    }
+    
+    public func nextGroup() -> DateGroup {
+        return DateGroup(date: Date(timeIntervalSince1970: self.getDateAsEpoch() + self.dc.getEpochInterval()),  dateConversion : dc)
+    }
+
+}
+
+extension DateGroup {
+    static func > (left: DateGroup, right: DateGroup) -> Bool {
+        return  left.getDateAsEpoch() > right.getDateAsEpoch()
+    }
+}
