f5b944250dfd1382a840ac61f90edcc33d7f4790
Dennis Schafroth <dennis@schafroth.dk>
2018-08-16 21:38:01 +0200

    New LineStat
    Former-commit-id: 1558b45f492eecf054efab5b38e4c2fe3cd87c0c

---
+++ Sources/MailLogCount/LineStat.swift
@@ -0,0 +1,59 @@
+//
+//  LineStat.swift
+//  Basic
+//
+//  Created by Dennis Schafroth on 16/08/2018.
+//
+
+import Foundation
+
+
+class LineStat : CustomStringConvertible {
+    var total = 0
+    var matched = 0
+    
+    var description : String {
+        var description = ""
+        description += "\(self.matched)\t"
+        description += "\(self.total)"
+        return description
+    }
+
+    init() {
+        total = 0
+        matched = 0
+    }
+
+    func reset() {
+        total = 0
+        matched = 0
+    }
+
+    public func incTotal() {
+        total += 1
+    }
+
+    public func getTotal() -> Int {
+        return total
+    }
+
+    public func addMatches(_ match : Int) {
+        matched  += match
+    }
+
+    public func getMatched() -> Int {
+        return matched
+    }
+
+    public func printStat(_ string : String) {
+        print(description + "\t" + string)
+    }
+}
+
+class ProcentLineStat : LineStat {
+    
+    override func printStat(_ string : String) {
+        print("" + String((100*matched/total)) + "%\t" + string)
+    }
+
+}
